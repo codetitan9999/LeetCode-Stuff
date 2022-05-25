3b9965e8c140dadde7d77761428fb9583940bc13
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2022-05-25 14:45:13 +0530

    Time: 46 ms (10.90%), Space: 18 MB (20.97%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -3,9 +3,8 @@ public:
     int missingNumber(vector<int>& nums) {
         int xoro=0;
         for(int i=0;i<nums.size();i++)
-            xoro^=nums[i];
-        for(int i=1;i<=nums.size();i++)
-            xoro^=i;
+            xoro^=nums[i]^(i+1);
+        
         return xoro;
         
     }
