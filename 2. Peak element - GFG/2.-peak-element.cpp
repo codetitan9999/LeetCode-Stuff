1f13a097b7b07f7a8c78e33e6af4d90d75d089c6
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2022-06-22 13:04:34 +0530

    Added solution - LeetHub

--- 2. Peak element - GFG/2.-peak-element.cpp
+++ 2. Peak element - GFG/2.-peak-element.cpp
@@ -22,12 +22,14 @@ class Solution
            {
                 int mid=(low+high)/2;
               
-                if((mid==0||arr[mid]>arr[mid-1])&&(mid==n-1||arr[mid]>arr[mid+1]))
+                if(mid==0&&arr[mid]>arr[mid+1])
+                return mid;
+                else if(mid==n-1&&arr[mid]>arr[mid-1])
                 return mid;
                 else if(mid>0&&arr[mid]<arr[mid-1])
-                {
                     high=mid-1;
-                }
+                else if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1])
+                return mid;
                 else
                 low=mid+1;
            }
