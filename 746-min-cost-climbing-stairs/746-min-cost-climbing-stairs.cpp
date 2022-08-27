f77dbf962bdc038650ebfd51b12505b92429f4f2
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2022-08-28 01:01:14 +0530

    Time: 109 ms (5.13%), Space: 240.2 MB (5.07%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -11,6 +11,12 @@ public:
         return mat[n];
     }
     int minCostClimbingStairs(vector<int>& cost) {
+        if(cost.size()==0)
+            return 0;
+        else if(cost.size()==1)
+            return cost[0];
+        else if(cost.size()==2)
+            return min(cost[0],cost[1]);
         memset(mat,-1,sizeof mat);
         int ans=min(fun(cost, cost.size()-1), fun(cost,cost.size()-2));
         return ans;
