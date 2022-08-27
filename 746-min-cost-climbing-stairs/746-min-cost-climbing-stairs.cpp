2679ce9ce03ce9c9ee44f8e8ea1364ccecac1aae
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2022-08-28 01:12:51 +0530

    Time: 11 ms (53.97%), Space: 13.9 MB (9.68%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int mat[1001];
     int fun(vector<int> arr,int n) {
-        if(n<0)
-            return 0;
-        if(n==0||n==1)
-            return arr[n];
-      if(mat[n]==-1)
-        mat[n]=min(arr[n]+fun(arr,n-1), arr[n]+fun(arr,n-2));
+        int mat[n+1];
+        mat[0]=arr[0];
+        mat[1]=arr[1];
+        for(int i=2;i<=n;i++) {
+            mat[i]=arr[i]+ min(mat[i-1],mat[i-2]);
+}
         return mat[n];
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        if(cost.size()==0)
+        int n=cost.size();
+        if(n==0)
             return 0;
-        else if(cost.size()==1)
+        if(n==1)
             return cost[0];
-        else if(cost.size()==2)
-            return min(cost[0],cost[1]);
-        memset(mat,-1,sizeof mat);
-        int ans=min(fun(cost, cost.size()-1), fun(cost,cost.size()-2));
-        return ans;
+        if(n==2)
+            return min(cost[1],cost[0]);
+        
+        return min(fun(cost,n-1), fun(cost,n-2));
+        
     }
 };
