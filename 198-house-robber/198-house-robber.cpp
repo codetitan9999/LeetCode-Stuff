0553ea783c26be6975f3788e45cb9b9e5dacecbf
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2022-08-28 21:08:32 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.56%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-   int mat[101];
     int fun(vector<int> arr, int n) {
         if(n<0)
             return 0;
-        if(n==0)
-            return arr[n];
-        if(mat[n]==-1)
-        mat[n]=max(arr[n]+fun(arr,n-2), fun(arr,n-1));
-        return mat[n];
+        int a[n+1];
+        a[0]=arr[0];
+        a[1]=max(arr[0],arr[1]);
+        for(int i=2;i<=n;i++) {
+            a[i]=max(arr[i]+a[i-2], a[i-1]);
+        }   
+        return a[n];
        
     }
     int rob(vector<int>& nums) {
@@ -17,7 +18,7 @@ public:
             return 0;
         if(n==1)
             return nums[0];
-        memset(mat, -1 , sizeof mat);
+
         return fun(nums,n-1);
         
     }
