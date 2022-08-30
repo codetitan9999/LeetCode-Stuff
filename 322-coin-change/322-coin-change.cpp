d77ba13fc2fe3f333ab439ebb1f418ca75e26f48
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2022-08-30 14:39:39 +0530

    Time: 112 ms (73.27%), Space: 10.4 MB (91.81%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    int fun(vector<int> arr, int sum, int n) {
+        
+      int mat[n+1][sum+1];
+        for(int i=0;i<=n;i++) 
+            mat[i][0]=0;
+        for(int j=0;j<=sum;j++) {
+            mat[0][j]=INT_MAX-1;
+        }
+         
+        for(int j=1;j<=sum;j++) {
+            if(j%arr[0]==0)
+                mat[1][j]=j/arr[0];
+            else
+                mat[1][j]=INT_MAX-1;
+        }
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(arr[i-1]<=j) 
+                    mat[i][j]=min(1+mat[i][j-arr[i-1]], mat[i-1][j]);
+                else
+                    mat[i][j]=mat[i-1][j];
+        
+    }
+    }
+
+        return mat[n][sum];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+       int ans=fun(coins, amount, coins.size());
+        if(ans==INT_MAX-1)
+            return -1;
+        return ans;
+    }
+};
