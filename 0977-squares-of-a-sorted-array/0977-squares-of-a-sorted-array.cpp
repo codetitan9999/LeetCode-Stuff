baaa6ed283601cb5dbd8cb8d0e06355719aaded2
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2022-10-30 17:49:09 +0530

    Time: 73 ms (52.75%), Space: 25.7 MB (99.80%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> ans;
-        int p1=0,p2=0;
-        while(p1<nums.size() && nums[p1]<0)
-            p1++;
-        p2=p1;
-        while(p2<nums.size() && nums[p2]==0)
-            p2++;
-        for(int i=p1;i<p2;i++)
-            ans.push_back(0);
-       p1--;
-        while(p2<nums.size() && p1>=0 ) {
-            if(abs(nums[p2])<abs(nums[p1])) {
-                ans.push_back(nums[p2]*nums[p2]);
-                p2++;
+        vector<int> ans(nums.size());
+        int p1=0,p2=nums.size()-1;
+        for(int i=nums.size()-1;i>=0; i--) {
+            if(abs(nums[p1])> abs(nums[p2])) {
+                ans[i]=nums[p1]*nums[p1];
+                p1++;
             }
-             else {
-                 ans.push_back(nums[p1]*nums[p1]);
-                 p1--;
+            else
+            {
+                ans[i]=nums[p2]*nums[p2];
+                p2--;
             }
-              }
-        while(p2<nums.size()) {
-            ans.push_back(nums[p2]*nums[p2]);
-                p2++;
-        }
-        while(p1>=0)
-        {
-            ans.push_back(nums[p1]*nums[p1]);
-            p1--;
         }
         return ans;
     }
