a3c146f796a53a3954acda76fe1403c63fc7a9ef
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2023-01-21 02:04:46 +0530

    Added solution - LeetHub

---
+++ Maximum Stone Removal - GFG/maximum-stone-removal.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class DisjointSet {
+     public:
+    vector<int> rank,size,parent;
+   
+    DisjointSet(int n) {
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++)
+        parent[i]=i;
+    }
+    int getParent(int u) {
+        if(parent[u]==u)
+            return u;
+        return parent[u]=getParent(parent[u]);
+    }
+    void UnionByRank(int u, int v){
+        int ulp_u=getParent(u);
+        int ulp_v=getParent(v);
+        if(ulp_u==ulp_v)
+            return ;
+        if(rank[ulp_u]<rank[ulp_v]) {
+            parent[ulp_u]=ulp_v;
+            
+        }
+        else if(rank[ulp_v]<rank[ulp_u]) {
+            parent[ulp_v]=ulp_u;
+        }
+        else {
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
+        
+    }
+    void UnionBySize(int u, int v) {
+        int ulp_u=getParent(u);
+        int ulp_v=getParent(v);
+        if(ulp_u==ulp_v)
+            return ;
+        if(size[ulp_v]<size[ulp_u]) {
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+        else  {
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+           
+        }
+        
+    }
+    
+};
+class Solution {
+  public:
+    int maxRemove(vector<vector<int>>& stones, int n) {
+        // Code here
+        int mrow=0;
+        int mcol=0;
+        for(auto it : stones) {
+           mrow=max(mrow,it[0]);
+           mcol=max(mcol,it[1]);
+        }
+        DisjointSet ds(mrow+mcol+1);
+        unordered_set<int> s;
+        for(auto it : stones) {
+            int row=it[0];
+            int col=it[1];
+            ds.UnionBySize(row,col+mrow+1);
+            s.insert(row);
+            s.insert(col+mrow+1);
+        }
+        int cnt=0;
+        for(auto i : s) {
+            if(ds.getParent(i)==i && ds.size[ds.getParent(i)]>1) {
+                cnt++;
+            }
+        }
+        return n-cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> adj;
+
+        for (int i = 0; i < n; ++i) {
+            vector<int> temp;
+            for (int i = 0; i < 2; ++i) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+
+        Solution obj;
+
+        cout << obj.maxRemove(adj, n) << "\n";
+    }
+}
+// } Driver Code Ends
