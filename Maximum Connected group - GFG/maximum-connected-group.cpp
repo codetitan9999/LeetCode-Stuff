ed40da4787f6454ef2c4ef16abbf3eeef762689c
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2023-06-27 18:50:09 +0530

    Added solution - LeetHub

--- Maximum Connected group - GFG/maximum-connected-group.cpp
+++ Maximum Connected group - GFG/maximum-connected-group.cpp
@@ -9,134 +9,103 @@ using namespace std;
 // User function Template for C++
 class DisjointSet {
     public:
-    vector<int> rank,size,parent;
-    
+    vector<int> parent,rank , size;
     DisjointSet(int n) {
-        rank.resize(n+1,0);
-        size.resize(n+1,1);
         parent.resize(n+1);
-        for(int i=0;i<=n;i++)
-        parent[i]=i;
+        rank.resize(n+1 , 0);
+        size.resize(n+1 , 1);
+        for(int i = 0 ; i <=n ; i++)
+        parent[i] = i;
     }
-    int getParent(int u) {
-        if(parent[u]==u)
-            return u;
-        return parent[u]=getParent(parent[u]);
+    int findUPar(int u) {
+        if(parent[u] == u)
+        return u;
+        return parent[u] = findUPar(parent[u]);
     }
-    void UnionByRank(int u, int v){
-        int ulp_u=getParent(u);
-        int ulp_v=getParent(v);
-        if(ulp_u==ulp_v)
-            return ;
-        if(rank[ulp_u]<rank[ulp_v]) {
-            parent[ulp_u]=ulp_v;
-            
-        }
-        else if(rank[ulp_v]<rank[ulp_u]) {
-            parent[ulp_v]=ulp_u;
+    void unionBySize(int u , int v) {
+        int ult_par_u = findUPar(u);
+        int ult_par_v = findUPar(v);
+        if(ult_par_u ==  ult_par_v)
+        return;
+        if(size[ult_par_u] < size[ult_par_v]) {
+            parent[ult_par_u] = ult_par_v;
+            size[ult_par_v]+=size[ult_par_u];
         }
         else {
-            parent[ulp_v]=ulp_u;
-            rank[ulp_u]++;
-        }
-        
+            parent[ult_par_v] = ult_par_u;
+            size[ult_par_u]+= size[ult_par_v];
+        } 
     }
-    void UnionBySize(int u, int v) {
-        int ulp_u=getParent(u);
-        int ulp_v=getParent(v);
-        if(ulp_u==ulp_v)
-            return ;
-        if(size[ulp_v]<size[ulp_u]) {
-            parent[ulp_v]=ulp_u;
-            size[ulp_u]+=size[ulp_v];
+    void unionByRank(int u , int v) {
+        int ult_par_u = findUPar(u);
+        int ult_par_v = findUPar(v);
+        if(ult_par_u ==  ult_par_v)
+        return;
+        if(rank[ult_par_u] < rank[ult_par_v]) {
+            parent[ult_par_u] = ult_par_v;
         }
-        else  {
-            parent[ulp_u]=ulp_v;
-            size[ulp_v]+=size[ulp_u];
-           
+        else if(rank[ult_par_u] > rank[ult_par_v]){
+            parent[ult_par_v] = ult_par_u;
+
         }
-        
+        else {
+            rank[ult_par_u]++;
+            parent[ult_par_v] = ult_par_u;
+        } 
     }
-    
 };
 class Solution {
   public:
-    void dfs(int row,int col , vector<vector<int>> & vis, vector<vector<int>> & grid, DisjointSet & ds) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vis[row][col]=1;
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
-         for(int k=0;k<4;k++) {
-            int r=row+dr[k];
-            int c=col+dc[k];
-            if(r>=0 && c>=0 && r<n && c<m &&grid[r][c]==1 && vis[r][c]!=1) {
-                    ds.UnionBySize(row*m+col, r*m+c); 
-                    dfs(r,c,vis,grid,ds);
-                }
-            }
-            
-        }
-        
-
     int MaxConnection(vector<vector<int>>& grid) {
         // code here
-      
-        int n=grid.size();
-        
-        int m=grid[0].size();
-        
-        DisjointSet ds(n*m);
-        
-        int dr[]={-1,0,1,0};
-        
-        int dc[]={0,1,0,-1};
-        vector<vector<int>> vis(n, vector<int> (m,0));
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(vis[i][j]==0 && grid[i][j]==1) {
-                    dfs(i,j,vis,grid,ds);
-                }
-            }
-        }
-        int ans=1;
-        
-   
-        
-        for(int i=0;i<n;i++) {
-            
-            for(int j=0;j<m;j++) {
+        int n = grid.size();
+        DisjointSet ds(n*n);
+        int delrow[] = {0 , 0 , -1, 1};
+        int delcol[] = {-1 ,1 , 0 , 0};
+        for(int i = 0 ; i < n ; i++) 
+        {
+            for(int j = 0 ; j < n ; j++) {
                 
-                if(grid[i][j]==0) {
-                    
-                    int count=0;
-                    
-                    unordered_set<int> sety;
-                    
-                    for(int k=0;k<4;k++) {
-                        
-                        int r=i+dr[k];
-                        
-                        int c=j+dc[k];
-                        
-                        if(r>=0 && c>=0 && r<n && c<m &&grid[r][c]==1) {
-                            
-                            if(sety.find(ds.getParent(r*m+c)) == sety.end()) {
-                                
-                            count+=ds.size[ds.getParent(r*m+c)];
-                            
-                            sety.insert(ds.getParent(r*m+c));
-                            
-                            }
-                        }
+                if(grid[i][j] == 0)
+                continue;
+                for(int k = 0 ; k < 4 ; k++) {
+                    int row = i + delrow[k];
+                    int col = j + delcol[k];
+                    if(row>=0 && col >=0 && row < n && col < n && grid[row][col] == 1 ) {
+                        ds.unionBySize(row*n + col , i*n + j);
                     }
-                    
-                    ans=max(ans,count+1);
                 }
             }
         }
+    
+        int ans = 0 ;
         
+        for(int i = 0 ; i < n ; i++) {
+           
+            for(int j = 0 ; j < n ; j++) {
+                if(grid[i][j] == 1)
+                continue;
+                unordered_set<int> st;
+                for(int k = 0 ; k < 4 ; k++) {
+                    int row = i + delrow[k];
+                    int col = j + delcol[k];
+                    if(row >= 0 && col >= 0 && row < n && col < n && grid[row][col] == 1) {
+                        st.insert(ds.findUPar(row*n + col));
+                    }
+                }
+                int cnt =1;
+                for(auto it : st) {
+                   cnt+= ds.size[it];
+                }
+                ans= max(ans , cnt);
+                
+            }
+        }
+        for(int i = 0 ; i < n*n ;i++) {
+            ans = max( ans , ds.size[ds.findUPar(i)]);
+        }
         return ans;
+        
         
     }
 };
