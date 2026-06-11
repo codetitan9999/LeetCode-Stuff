c769ab50236310d2ba76610d12749032a1cdeada
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-06-11 18:12:50 +0530

    Time: 55 ms (14.81%), Space: 46.7 MB (14.27%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    bool direct(unordered_map<int , vector<int>> &adj , int u , int v) {
-        for(auto it : adj[u]) {
-            if(it == v)
-            return true;
-        }
-        return false;
+    bool direct(unordered_map<int , unordered_set<int>> &adj , int u , int v) {
+        return adj[u].count(v);
     }
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        unordered_map<int , vector<int>> adj;
+        unordered_map<int , unordered_set<int>> adj;
         for(auto it : roads) {
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+            adj[it[0]].insert(it[1]);
+            adj[it[1]].insert(it[0]);
         }
         int maxi = 0;
         for(int i = 0 ; i < n ; i++) {
