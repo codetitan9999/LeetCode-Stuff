b9c565553b9e1b9412e18da919fa13a57229e3ff
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-06-15 16:50:10 +0530

    Time: 1326 ms (5.79%), Space: 119.4 MB (37.09%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
+    vector<int> nnum = {0 , 1};
+    vector<int> dnum = {-1 , 0};
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        priority_queue<pair<double,pair<int,int>>> pq;
-        for(int i = 0 ; i < arr.size() ; i++) {
-            for(int j = i+1 ; j < arr.size() ; j++) {
-                pq.push({ ((double)arr[i] / (double)arr[j]) ,{i , j}});
-                if(pq.size() > k) pq.pop();
+        int n = arr.size();
+        priority_queue<pair<double , pair<int,int>> , vector<pair<double , pair<int,int>>> , greater<pair<double , pair<int,int>>>> pq;
+        set<pair<int,int>> vis;
+        pq.push({(double)arr[0]/(double)arr[n-1], {0 , n-1}});
+        int cnt = 0;
+        vis.insert({0 , n-1});
+        while(!pq.empty()) {
+            int num = pq.top().second.first;
+            int denum = pq.top().second.second;
+            double val = pq.top().first;
+            pq.pop();
+            cnt++;
+            if(cnt == k) {
+                return {arr[num] , arr[denum]};
+            }
+            for(int i = 0 ; i < 2 ; i++) {
+                int delnum = num + nnum[i];
+                int deldnum = denum + dnum[i];
+                if(delnum < n && deldnum > delnum && !vis.count({delnum , deldnum})) {
+                    pq.push({(double)arr[delnum]/(double)arr[deldnum] , {delnum , deldnum}});
+                    vis.insert({delnum , deldnum});
+                }
             }
         }
-        return {arr[pq.top().second.first], arr[pq.top().second.second]};
+        return {};
 
 
-
-        
     }
 };
