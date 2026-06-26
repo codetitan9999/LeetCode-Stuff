e0cd445c5801188ef76d7559cd118d7f5dbc7d1c
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-06-26 14:21:20 +0530

    Time: 9 ms (55.23%), Space: 21.1 MB (30.58%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    bool pal(string s) {
-        int i = 0 , j = s.length() -1;
-        while( i < j) {
-            if(s[i] != s[j])
-            return false;
-            i++;
-            j--;
+    int expand(string s , int start, int end) {
+        int ans = 0;
+        while(start >=0 && end < s.length() && s[start] == s[end]) {
+            start--;
+            end++;
+            ans++;
         }
-        return true;
-    }
-    int f(int ind , string & s) {
-        if(ind == s.length()) return 0;
-        int ans =0;
-        for(int i = ind ; i < s.length() ; i++) {
-            string x = s.substr(ind , i -ind +1);
-            if(pal(x)) {
-                ans++;
-            }
-        }
-        return ans + f(ind+1 , s);
+        return ans;
+
     }
     int countSubstrings(string s) {
-        return f(0 , s);
-        
+        int ans = 0;
+        for(int i = 0 ; i < s.length() ; i++) {
+            ans += expand(s , i , i);
+            ans += expand(s , i , i+1);
+        }
+        return ans;
     }
 };
