57711f9a823425a2dff9ecf6c18ce93b9ccf8913
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-07-09 17:57:54 +0530

    Time: 80 ms (16.44%), Space: 240.7 MB (18.63%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,20 +1,50 @@
+class ds {
+    public:
+    vector<int> size , parent;
+    ds(int n) {
+        size.resize(n+1 , 1);
+        parent.resize(n+1);
+        for(int i = 0 ; i <= n ; i++) {
+            parent[i] = i;
+        }
+    }
+    int fpar(int u) {
+        if(parent[u] == u) return u;
+        return parent[u] = fpar(parent[u]);
+    }
+    void ubs(int u , int v) {
+        int ul_u = fpar(u);
+        int ul_v = fpar(v);
+        if(ul_u == ul_v) return ;
+        if(size[ul_u] > size[ul_v]) {
+            parent[ul_v] = ul_u;
+            size[ul_u] += size[ul_v];
+        } else {
+            parent[ul_u] = ul_v;
+            size[ul_v] += size[ul_u];
+        }
+    }
+};
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<int> component(n, 0);
-        int compNo = 0;
-        for(int i = 1; i < n; i++){
-            if(nums[i] - nums[i - 1] > maxDiff){
-                compNo++;
+        vector<bool> ans;
+        ds ds(n);
+        for(int i = 0 ; i < n-1 ; i++) {
+            if((nums[i + 1] - nums[i]) <= maxDiff) {
+                ds.ubs(i , i+1);
             }
-            component[i] = compNo;
         }
-        vector<bool> sol;
-
-        for(auto &it : queries){
-            sol.push_back(component[it[0]] == component[it[1]]);
+        for(auto q : queries) {
+            int u = q[0];
+            int v = q[1];
+            if(ds.fpar(u) == ds.fpar(v)) {
+                ans.push_back(true);
+            } else {
+                ans.push_back(false);
+            }
         }
+        return ans;
 
-        return sol;
     }
 };
