950b938bdb22fec6d5bf91290c18d0bb9161db48
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-08-29 18:00:48 +0530

    Time: 19 ms (16.44%), Space: 105.9 MB (14.58%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
-    vector<int> delrow = {-1 , 1 , 0 , 0};
-    vector<int> delcol = {0 , 0 , 1 , -1};
-    int dfs(int row , int col , vector<vector<int>> & vis , vector<vector<int>>& grid) {
-        if(row < 0 || col < 0 || row >= grid.size() || col >= grid[0].size() || grid[row][col] == 0) return 1;
-
-        if(vis[row][col] == 1) return 0;
-        
+    vector<int> delrow = {-1 , 0 ,1 , 0};
+    vector<int> delcol = {0 , 1 , 0 , -1};
+    int f(int row , int col, vector<vector<int>> & grid , vector<vector<int>> & vis) {
+        int n = grid.size();
+        int m = grid[0].size();
         vis[row][col] = 1;
         int ans = 0;
+
         for(int i = 0 ; i < 4 ; i++) {
-            int drow = row+ delrow[i];
+            int drow = row + delrow[i];
             int dcol = col + delcol[i];
-            ans += dfs(drow , dcol , vis , grid);
+            if(drow <0 || dcol < 0 || drow >=n || dcol >= m || grid[drow][dcol] == 0) {
+                ans += 1;
+                continue;
+            } else if(!vis[drow][dcol]){
+                ans += f(drow , dcol , grid , vis);
+            }
+
         }
         return ans;
+
     }
     int islandPerimeter(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-
         vector<vector<int>> vis(n , vector<int> (m , 0));
-
-
-        for(int i = 0 ; i < n ; ++i) {
+        for(int i = 0 ; i < n ; i++) {
             for(int j = 0 ; j < m ; j++) {
-                if(grid[i][j] == 1) {
-                    return dfs(i , j , vis , grid);
+                if(!vis[i][j] & grid[i][j] == 1) {
+                    return f(i , j , grid , vis);
                 }
             }
         }
         return 0;
+
 
         
     }
