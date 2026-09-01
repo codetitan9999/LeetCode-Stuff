0f95ed0c3e49360171612b23874c796d66303b9a
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-09-01 12:38:41 +0530

    Time: 0 ms (100%), Space: 13.9 MB (49.9%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -3,27 +3,32 @@ using namespace std;
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        string s = "";
         int cnt = 1;
+        int index = 0;
         for(int i = 1 ; i < chars.size() ; i++) {
             if(chars[i-1] == chars[i]) {
                 cnt++;
             } else {
                 // TODO processing for single freq
-                s += (chars[i-1]);
+                chars[index++] = (chars[i-1]);
                 if(cnt != 1) {
-                    s += to_string(cnt); 
+                    string c = to_string(cnt);
+                    for(int p = 0 ; p < c.length() ; p++) {
+                        chars[index++] = c[p]; 
+                    }
                 } 
                 cnt = 1;
             }
         }
-        s += chars[chars.size()-1];
-        if(cnt != 1)  s += to_string(cnt); 
-        
-        for(int i = 0 ; i < s.length() ; i++) {
-            chars[i] = s[i];
+        chars[index++] = chars[chars.size()-1];
+        if(cnt != 1)  {
+            string c = to_string(cnt);
+            for(int p = 0 ; p < c.length() ; p++) {
+                chars[index++] = c[p]; 
+            }
         }
-        return s.length();
+
+        return index;
 
    
 
