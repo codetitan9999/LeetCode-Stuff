c8b7c069f96bc56405f77bd59e362632fd56bf8d
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-09-03 18:52:22 +0530

    Time: 31 ms (85.37%), Space: 10.8 MB (81.47%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
-    bool f(int i , int j , string a , string b, vector<vector<int>> & dp) {
-        //base case
-        
-        if(i == 0 && j == 0) return true;
 
-        if(j == 0) return false;
-
-        if(i == 0) {
-            for(int k = 0 ; k < j ; k++) {
-                if(b[k] != '*') return false;
-            }
-            return true;
-        }
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(a[i-1] == b[j-1] || (b[j-1] == '?')) {
-            return dp[i][j] = f(i-1 , j-1 , a, b, dp);
-        } else if(b[j-1] == '*') {
-            return dp[i][j] = f(i , j-1 , a, b , dp) || f(i-1 , j , a , b , dp);
-        } else {
-            return dp[i][j] = false;
-        }
-    }
     bool isMatch(string a, string b) {
         int n = a.length() , m = b.length();
+        vector<int> prev(m+1 , false);
+        vector<int> curr(m+1, false);
 
-        vector<vector<int>> dp(n+1 , vector<int> (m+1 , -1));
 
         
-        for(int i = 0 ; i <= n ; i++) {
-            dp[i][0] = false;
-        }
+
 
         for(int j = 0 ; j <= m ; j++) {
             bool val = true;
@@ -42,25 +18,26 @@ public:
                     break;
                 }
             }
-            dp[0][j] = val;
+            prev[j] = val;
         }
-        dp[0][0] = true;
+        // prev[0] = false;
 
 
         for(int i = 1 ; i <= n ; i++) {
             for(int j = 1 ; j <= m ; j++) {
 
                 if(a[i-1] == b[j-1] || (b[j-1] == '?')) {
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 } else if(b[j-1] == '*') {
-                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                    curr[j] = curr[j-1] || prev[j];
                 } else {
-                    dp[i][j] = false;
+                    curr[j] = false;
                 }
             }
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
 
         
     }
