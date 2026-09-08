0063a2c288efcf07d7d939a428938601b294dd3b
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-09-08 11:54:41 +0530

    Time: 0 ms (100%), Space: 13.9 MB (57.06%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long long xoro = 0;
+        for(auto x : nums) xoro ^= x;
+
+        long long bit = (xoro & ((~xoro)+1));
+        cout<<bit<<endl;
+
+        long long a = 0 , b = 0;
+
+        for(auto x : nums) {
+            if((x&bit)) {
+                a = a ^ x;
+            } else {
+                b = b ^ x;
+            }   
+        }
+        return {(int)a, (int)b};
+    }
+};
