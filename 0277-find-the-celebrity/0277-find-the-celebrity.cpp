8c9e3dc4d57795e64ec946162a09397c0b02fc8b
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-09-16 15:55:20 +0530

    Time: 67 ms (31.42%), Space: 13.8 MB (35.49%) - LeetHub

---
+++ 0277-find-the-celebrity/0277-find-the-celebrity.cpp
@@ -0,0 +1,28 @@
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int i = 0 , j = n-1;
+
+        while(i < j) {
+            if(knows(i , j) && knows(j , i)) {
+                i++;
+                j--;
+            } else if(knows(i , j)){
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        for(int val = 0 ; val < n ; val++) {
+            if(val ==i) continue;
+            if(knows(i , val)) return -1;
+            if(!knows(val , i)) return -1;
+        }
+        return i;
+
+    }
+};
