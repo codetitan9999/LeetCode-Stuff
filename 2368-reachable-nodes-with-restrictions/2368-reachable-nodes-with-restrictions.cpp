906632166d402bf3c79272c6d921f590825b00bf
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-09-18 13:25:55 +0530

    Time: 104 ms (93.51%), Space: 144.3 MB (93.39%) - LeetHub

---
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -0,0 +1,47 @@
+class DS {
+    public:
+    vector<int> size;
+    vector<int> parent;
+    DS(int n) {
+        size.resize(n , 1);
+        parent.resize(n);
+        for(int i = 0 ; i < n ; i++) parent[i] = i;
+    }
+
+    int findParent(int u) {
+        if(parent[u] == u) return u;
+
+        return parent[u] = findParent(parent[u]);
+    }
+
+    void ubs(int u , int v) {
+        int ul_u = findParent(u);
+        int ul_v = findParent(v);
+        if(ul_u == ul_v) return;
+
+        if(size[ul_u] > size[ul_v]) {
+            size[ul_u] += size[ul_v];
+            parent[ul_v] = ul_u;
+        } else {
+            size[ul_v] += size[ul_u];
+            parent[ul_u] = ul_v;
+        }
+    }
+
+};
+
+
+class Solution {
+public:
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+        DS ds(n);
+        unordered_set<int> st (restricted.begin() , restricted.end());
+        for(auto it : edges) {
+            int u = it[0];
+            int v = it[1];
+            if(!st.count(u) && !st.count(v)) ds.ubs(u , v);
+        }
+
+        return ds.size[ds.findParent(0)];
+    }
+};
