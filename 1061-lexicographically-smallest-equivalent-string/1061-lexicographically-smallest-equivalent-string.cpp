60795f4b9f1cb6dea4b2554164070f7a92016611
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-09-22 18:26:09 +0530

    Time: 26 ms (5.1%), Space: 17.7 MB (5.01%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,31 +1,67 @@
-class Solution {
-public:
-    void dfs(char node , unordered_set<char> & vis ,unordered_map<char, vector<char>> & adj, char & ans) {
-        vis.insert(node);
-        if(ans > node) ans = node;
-        for(auto it : adj[node]) {
-            if(!vis.count(it)) {
-                dfs(it , vis , adj , ans);
-            }
+class DS {
+    public:
+    vector<int> parent, size , rank;
+    DS(int n) {
+        parent.resize(n);
+        size.resize(n , 1);
+        rank.resize(n , 0);
+        for(int i = 0 ; i < n ; i++) {
+            parent[i] = i;
         }
     }
-        //    for(int i = 0 ; i < s1.length() ; i++) {
-        //     adj[s1[i]-'a'].push_back(s2[i]-'a');
-        //     adj[s2[i]-'a'].push_back(s1[i]-'a');
-        // }
+    int findPar(int u) {
+        if(parent[u] == u) return u;
+        return parent[u] = findPar(parent[u]);
+    }
+
+    void ubs(int u , int v) {
+        int ul_u = findPar(u);
+        int ul_v = findPar(v);
+        if(ul_u == ul_v) return;
+        if(size[ul_u] > size[ul_v]) {
+            parent[ul_v] = ul_u;
+            size[ul_u] += size[ul_v];
+        } else {
+            parent[ul_u] = ul_v;
+            size[ul_v] += size[ul_u];
+        }
+    }
+    void unionByRank(int u , int v) {
+        int ul_u = findPar(u);
+        int ul_v = findPar(v);
+        if(ul_u == ul_v) return;
+        if(rank[ul_u] == rank[ul_v]) {
+            parent[ul_v] = ul_u;
+            rank[ul_u]++;
+        } else if(rank[ul_u] > rank[ul_v]) {
+            parent[ul_v] = ul_u;
+        } else {
+            parent[ul_u] = ul_v;
+        }
+    }
+};
+
+class Solution {
+public:
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        unordered_map<char, vector<char>> adj;
+        DS ds(26);
         for(int i = 0 ; i < s1.length() ; i++) {
-            adj[s1[i]].push_back(s2[i]);
-            adj[s2[i]].push_back(s1[i]);
-        }
-        string ans = "";
+            int u = s1[i] - 'a';
+            int v = s2[i] - 'a';
+            ds.ubs(u , v);
+        }    
+
+        unordered_map<int , set<int>> st;
+
+        for(int i = 0 ; i < 26 ; i++) {
+            st[ds.findPar(i)].insert(i);
+        } 
+
         for(int i = 0 ; i < baseStr.length() ; i++) {
-            unordered_set<char> vis;
-            char x = baseStr[i];
-            dfs(baseStr[i] , vis , adj , x);
-            ans.push_back(x);
+            int root = ds.findPar(baseStr[i]- 'a');
+            set<int> k = st[root];
+            baseStr[i] = 'a' + (*k.begin());
         }
-        return ans;
+        return baseStr;       
     }
 };
