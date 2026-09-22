9bdf357c41c0abbcfc0d34dccc5a225fa4f87258
codetitan9999 <100030272+codetitan9999@users.noreply.github.com>
2026-09-22 16:07:44 +0530

    Time: 84 ms (5.23%), Space: 78.5 MB (7.19%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    bool dfs(int node, int c, vector<int> &color, vector<int>  adj[]) {
-        color[node]=c;
+    bool dfs(int node , int col ,  unordered_map<int, vector<int>> & adj ,vector<int> & color) {
+        color[node] = col;
+
         for(auto it : adj[node]) {
-            if(color[it]==-1) {
-                if(!dfs(it,!color[node],color,adj))
-                return false;
-            }
-            else if(color[it]==color[node])
-            return false;
+            if(color[it] == -1) {
+                if(!dfs(it , 1-col , adj , color)) return false;
+            } else if(color[it] == col) return false;
         }
         return true;
     }
-    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        vector<int> adj[n+1];
-        for(auto it : dislikes) {
-            int u=it[0];
-            int v=it[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+    bool possibleBipartition(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        for(auto it : edges) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
-        vector<int> color(n+1,-1);
-        for(int i=1;i<=n;i++) {
-            if(color[i]==-1) {
-                if(!dfs(i,0,color,adj))
-                return false;
+        vector<int> color(n+1 , -1);
+        bool ans = true;
+        for(int i = 1; i <= n ; i++) {
+            if(color[i] == -1) {
+                if(!dfs(i , 0,  adj , color)) return false;
             }
         }
         return true;
-     
+        
     }
 };
